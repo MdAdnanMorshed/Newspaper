599e7e5e701cbc6ca4b43062d749698c39486fef
MdAdnanMorshed <adnanmorshed118@gmail.com>
2023-05-27 16:34:07 +0600

    internet connect but not fully working

--- lib/main.dart
+++ lib/main.dart
@@ -42,24 +42,17 @@ void main() async {
 Future<void> execute(
   InternetConnectionChecker internetConnectionChecker,
 ) async {
-
   print('''The statement 'this machine is connected to the Internet' is: ''');
   final bool isConnected = await InternetConnectionChecker().hasConnection;
   // ignore: avoid_print
   print(
     isConnected.toString(),
   );
-  // returns a bool
 
-  // We can also get an enum instead of a bool
-  // ignore: avoid_print
   print(
     'Current status: ${await InternetConnectionChecker().connectionStatus}',
   );
-  // Prints either InternetConnectionStatus.connected
-  // or InternetConnectionStatus.disconnected
 
-  // actively listen for status updates
   final StreamSubscription<InternetConnectionStatus> listener =
       InternetConnectionChecker().onStatusChange.listen(
     (InternetConnectionStatus status) {
@@ -70,13 +63,13 @@ Future<void> execute(
           break;
         case InternetConnectionStatus.disconnected:
           // ignore: avoid_print
-          print('You are disconnected from the internet.');
+          Get.snackbar('Alert', 'Internet has occur!',
+              backgroundColor: Colors.red, colorText: Colors.white);
           break;
       }
     },
   );
 
-  // close listener after 30 seconds, so the program doesn't run forever
-  await Future<void>.delayed(const Duration(seconds: 30));
+  await Future<void>.delayed(const Duration(seconds: 1));
   await listener.cancel();
 }
